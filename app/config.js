a1541cd9930bebaf5b4303e5b7f7bb85a2831411
Shashank Manohar <shashankmanohar1734@gmail.com>
2026-08-28 10:06:06 +0530

    update backend API base URL

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 
   (typeof window !== 'undefined' && window.location.hostname === 'localhost' 
     ? 'http://localhost:5000' 
-    : "https://teamexcellent-backend.vercel.app");
+    : "https://webflora-backend.vercel.app");
 
 export default API_BASE_URL;
